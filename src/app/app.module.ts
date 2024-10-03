41a28e5b37b4d1dfa827a4c369750467467b7dc6
DANIEL VITOR DUARTE <daniel.duarte2@GRANBERY.local>
2024-10-03 20:39:33 -0300

    componentes iniciais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './paginas/home/home.component';
+import { ConversorComponent } from './paginas/conversor/conversor.component';
+import { ListaComponent } from './paginas/lista/lista.component';
+import { HistoricoComponent } from './paginas/historico/historico.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ConversorComponent,
+    ListaComponent,
+    HistoricoComponent
   ],
   imports: [
     BrowserModule,

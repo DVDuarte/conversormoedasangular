04f5241330093bf80cd9ad9c08d57bee1f273eb0
DANIEL VITOR DUARTE <daniel.duarte2@GRANBERY.local>
2024-10-03 22:11:16 -0300

    update rotas

--- src/app/services/currency.service.ts
+++ src/app/services/currency.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CurrencyService {
 
-  constructor() { }
+  private apiUrl = 'https://www.exchangerate-api.com/2ff50344af86d60fabdf8421';
+
+  constructor(private http: HttpClient) { }
+
+  getCurrencies(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/v4/latest/USD`);
+  }
 }

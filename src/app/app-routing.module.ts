5be0ef9ffb4ca229ce7371207ea2e7f992f64212
Daniel Duarte <danielvitasso@gmail.com>
2024-10-09 21:16:56 -0300

    configuaracao rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConversorComponent } from './paginas/conversor/conversor.component';
+import { HomeComponent } from './paginas/home/home.component';
+import { HistoricoComponent } from './paginas/historico/historico.component';
+import { ListaComponent } from './paginas/lista/lista.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'conversor', component: ConversorComponent},
+  { path: 'historico', component: HistoricoComponent},
+  { path: 'lista', component: ListaComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
